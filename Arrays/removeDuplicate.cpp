4797bfac5b37b69f707a5f863d6e15500863da1d
Tanpreet Singh Jolly <tanpreet2002@gmail.com>
2024-02-22 13:57:10 +0000

    remove duplicated from arr

---
+++ Arrays/removeDuplicate.cpp
@@ -0,0 +1,16 @@
+//brute forcem -> add elements to hashset and then move to new array
+
+//optimal -> push only if the previous element wasn't same
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j = 1;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] != nums[i - 1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};
