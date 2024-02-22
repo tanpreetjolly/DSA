6b2f94ad111352ee76028f9764bdca38c2e1b1a0
Tanpreet Singh Jolly <tanpreet2002@gmail.com>
2024-02-22 13:57:30 +0000

    rotate arr

---
+++ Arrays/rotateArray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void reverse(vector<int> &nums, int start, int end){
+        while(start<end){
+            swap(nums[start], nums[end]);
+            start++;
+            end--;
+        }
+    }
+
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k= k%n;
+        reverse(nums, 0, n-k-1);
+        reverse(nums, n-k, n-1);
+        reverse(nums, 0, n-1);
+    }
+};
+
+//reverse last n-k elements
+// reverse first n-k-1 elements
+//reverse whole array
